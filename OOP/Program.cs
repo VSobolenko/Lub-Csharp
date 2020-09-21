a28cd729f7c456d5740cf42b8845b3a281e17d1d
Vladislav <v.sobolenko.vs@gmail.com>
2020-09-21 21:35:41 +0300

    test

---
+++ OOP/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A classA = new A();
+            Console.WriteLine(classA.c);
+        }
+    }
+    class A
+    {
+        private int a = 3;
+        private int b = 7;
+        public int c
+        {
+            get
+            {
+                return a %= (a + b);
+            }
+            set
+            {
+                a = value;
+            }
+        }
+    }
+}
