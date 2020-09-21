84c3a91dac9961f6618aab3cb7f1d0dffd051116
Vladislav <v.sobolenko.vs@gmail.com>
2020-09-21 22:06:56 +0300

    C#

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -6,24 +6,7 @@ namespace OOP
     {
         static void Main(string[] args)
         {
-            A classA = new A();
-            Console.WriteLine(classA.c);
-        }
-    }
-    class A
-    {
-        private int a = 3;
-        private int b = 7;
-        public int c
-        {
-            get
-            {
-                return a %= (a + b);
-            }
-            set
-            {
-                a = value;
-            }
+            Console.WriteLine("Hello, world!");
         }
     }
 }
