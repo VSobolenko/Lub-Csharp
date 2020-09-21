b4d308214e82c0815ec5ebee76a8b6743c5ced21
Vladislav <v.sobolenko.vs@gmail.com>
2020-09-22 00:03:13 +0300

    Lab 2

---
+++ OOP/B.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class B : A
+    {
+        private int _d;
+
+        public B(int a, int b, int d) : base(a, b)
+        {
+            _d = d;
+        }
+        public B()
+        {
+        }
+
+        public int c2 
+        { 
+            get 
+            {
+                //if()
+                return _d;
+            } 
+            set
+            {
+                _d = value;
+            } 
+        }
+    }
+}
