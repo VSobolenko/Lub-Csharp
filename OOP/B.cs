cc055dd2e4a36e5930cae90505bf0c160455a84d
Vladislav <v.sobolenko.vs@gmail.com>
2020-09-22 12:33:56 +0300

    лаба 2 в ветке лаб 3

--- OOP/B.cs
+++ OOP/B.cs
@@ -4,7 +4,30 @@ using System.Text;
 
 namespace OOP
 {
-    class B
+    class B : A
     {
+        private int _d;
+        private int[] _arrayClass;
+
+        public B(int a, int b, int d) : base(a, b)
+        {
+            _d = d;
+        }
+        public B()
+        {
+        }
+
+        public int c2
+        {
+            get
+            {
+                //if()
+                return _d;
+            }
+            set
+            {
+                _d = value;
+            }
+        }
     }
 }
