b4a527f8b1bf9792e8d8899c753bb35a44fdf5d6
Vladislav <v.sobolenko.vs@gmail.com>
2021-03-14 18:58:57 +0300

    Create an inherited form

--- Windows Forms Lab/Program.cs
+++ Windows Forms Lab/Program.cs
@@ -17,6 +17,8 @@ namespace Windows_Forms_Lab
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Application.Run(new nForm());
+
         }
     }
 }
--- Windows Forms Lab/nForm.Designer.cs
+++ Windows Forms Lab/nForm.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Windows_Forms_Lab
 {
-    partial class Form1
+    partial class nForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,37 +29,27 @@ namespace Windows_Forms_Lab
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(76, 44);
-            this.button1.TabIndex = 0;
-            this.button1.Text = " Close Form";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.BackColor = System.Drawing.Color.Brown;
+            this.button1.Text = "Hello!!!";
+            this.button1.UseVisualStyleBackColor = false;
             // 
-            // Form1
+            // nForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Red;
+            this.BackColor = System.Drawing.Color.Blue;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Name = "nForm";
+            this.Text = "nForm";
+            this.Load += new System.EventHandler(this.nForm_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        protected System.Windows.Forms.Button button1;
     }
 }
-
