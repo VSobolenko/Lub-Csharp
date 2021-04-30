ae42b9bce000f94c9992b988661327e17ae86225
Vladislav <v.sobolenko.vs@gmail.com>
2021-04-30 11:33:42 +0300

    task1 - oval shape

---
+++ Additional/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Additional
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GraphicsPath path = new GraphicsPath();
+            /*
+            path.AddPolygon(new Point[] {
+                new Point(0,0),
+                new Point(0, this.Height),
+                new Point(this.Width, 0)
+            });
+            */
+            path.AddEllipse(0, 0, this.Width, this.Height);
+            Region region = new Region(path);
+            this.Region = region;
+        }
+    }
+}
