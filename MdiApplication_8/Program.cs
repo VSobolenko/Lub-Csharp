d138a0d8648963295b1ba47bd7cd8b5322f5a288
Vladislav <v.sobolenko.vs@gmail.com>
2021-05-20 12:42:13 +0300

    lab10

--- MdiApplication_8/Program.cs
+++ MdiApplication_8/Program.cs
@@ -8,9 +8,6 @@ namespace MdiApplication
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
